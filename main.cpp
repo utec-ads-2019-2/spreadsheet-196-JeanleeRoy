d284327db69a4796cef960fce50135d333069761
JeanleeRoy <jeanlee.barreto@utec.edu.pe>
2019-10-01 23:51:48 -0500

    Accepted

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <map>
 #include <cmath>
 #include <vector>
 #include <string>
@@ -9,7 +8,7 @@ struct Formula {
     int x;
     int y;
     string exp;
-    map<int,int> cels;
+    vector<pair<int,int>> cells;
     
     Formula(string exp, int x, int y)
     : exp{exp}, x{x}, y{y} {
@@ -18,8 +17,9 @@ struct Formula {
             if (exp[i] == '+') {
                 dCell(str);
                 str = "";
-            } else str += exp[i];
-        } dCell(str);
+            } else
+                str += exp[i];
+        }
     }
     void dCell(string str) {
         vector<int> col_num;
@@ -28,51 +28,70 @@ struct Formula {
         for (int i = 0; i < str.size(); ++i) {
             if ((int)str[i] > 64)
                 col_num.push_back((int)str[i]-64);
-            else  s_row += str[i];
-        } int n = col_num.size();
+            else
+                s_row += str[i];
+        }
+        int n = col_num.size();
         for (int i = 0; i < n; ++i)
             col += col_num[i]*(pow(26,n-i-1));
-        row = stoi(s_row)-1;
-        cels[row] = col;
-        cout << row << " " <<  cels[row] << endl;
+        cells.push_back(make_pair(stoi(s_row)-1,col));
     }
-
-    void celdas() {
-        for (auto i = cels.begin(); i != cels.end(); ++i)
-            cout << i->first << " " << i->second << endl;
-        cout << endl;
+    void execute(int** matrix) {
+        int result = 0;
+        for (int i = 0; i < cells.size(); ++i)
+            result += matrix[cells[i].first][cells[i].second];
+        matrix[x][y] = result;
     }
 };
-
-vector<Formula*> formulas;
 
 int main(int argc, char const *argv[]){
     int hojas = 0;
     cin >> hojas;
+
     for (int i = 0; i < hojas; ++i) {
+        vector<Formula*> formulas;
         int cols = 0, rows = 0;
-        cin >> rows >> cols;
+        cin >> cols >> rows;
         int **hoja = new int*[rows];
+        bool **check = new bool*[rows];
+
         for (int j = 0; j < rows; ++j) {
             hoja[j] = new int[cols];
+            check[j] = new bool[cols];
             for (int k = 0; k < cols; ++k) {
                 string value;
                 cin >> value;
                 if (value[0] == '=') {
+                    value += '+';
                     Formula* func = new Formula(value,j,k);
                     formulas.push_back(func);
-                } else 
+                    check[j][k] = 1;
+                } else {
                     hoja[j][k] = stoi(value);
+                    check[j][k] = 0;
+                }
             }
         }
-        /*for (auto x : formulas)
-            x->celdas();*/
-        /*
+        while (formulas.size() > 0) {
+            for (int i = 0; i < formulas.size(); ++i) {
+                bool indep = true;
+                for (auto c : formulas[i]->cells)
+                    if (check[c.first][c.second]) {
+                        indep = false;
+                        break;
+                    } 
+                if (indep) {
+                    formulas[i]->execute(hoja);
+                    check[formulas[i]->x][formulas[i]->y] = 0;
+                    formulas.erase(formulas.begin() + i);
+                }
+            }
+        }
         for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j)
+            for (int j = 0; j < cols-1; ++j)
                 cout << hoja[i][j] << " ";
-            cout << endl;
-        }*/
+            cout << hoja[i][cols-1] << endl;
+        }
     }
     
     return 0;
